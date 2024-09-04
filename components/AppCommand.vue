569b12bc4f999dfe34968b76a0ac0471d2a5f8ff
Elvis Sedić <elvis.sedic@gmail.com>
2024-09-04 11:14:05 +0200

    WIP

--- components/AppCommand.vue
+++ components/AppCommand.vue
@@ -19,16 +19,24 @@ function removeProperty(index) {
 // Use the `useFetch` function to fetch data from the API
 const { data: devices, error } = await useFetch('http://localhost:8080/rest/status');
 
+// Define select options for protocol dropdown
+const selectProtocol = [
+  { label: 'Send HTTP', value: 'http' },
+  { label: 'Send MQTT', value: 'mqtt' }
+]
+// Create a reactive state for protocol dropdown
+const selectedOption = ref('')
+
+// Table with devices list
+const devicesList = ref([devices[1]])
+
 // Format date
 const formattedData = computed(() => {
   return devices.value?.map(device => ({
     ...device,
-    lastSeen: dayjs(device.lastSeen).format('DD.MM.YYYY HH:mm')
+    lastSeen: dayjs(device.lastSeen).format('DD.MM.YYYY HH:mm:ss')
   })) || [];
 }); 
-
-// Protocol dropdown select 
-const selected = ref([devices[1]]);
 
 // Default sort
 const sort = ref({
@@ -56,45 +64,57 @@ watch(formattedData, (newData) => {
         <div v-else-if="devices">
             <!-- Protocol dropdown select -->
             <div class="select-command-container mb-4 flex">
-                <USelect size="sm" class="" placeholder="Select command..." :options="['Send HTTP', 'Send MQTT']" />
+                <USelect size="sm" class="" placeholder="Select command..." :options="selectProtocol"  v-model="selectedOption" />
             </div>
             <!-- Table with devices -->
-            <UTable :sort="sort" v-model="selected" :rows="formattedData" />           
+            <UTable :sort="sort" v-model="devicesList" :rows="formattedData" />           
             <!-- Form with additional data -->
             <div class="mx-auto mt-5 mb-5 max-w-7xl">
                 <div class="form-container">
-                    <div class="topic-container flex mb-4">
-                        <label class="text-sm font-medium text-gray-700 dark:text-gray-200" for="topic">Topic:
-                            <UInput name="topic" />
-                        </label>
-                    </div>
-                    <div class="retain-container mb-4">
-                        <label for="retain">
-                            <UCheckbox name="retain" label="Retain" />
-                        </label>
-                    </div>
-                    <div class="properties-element mb-4">
-                        <p>Properties:</p>
-                        <div class="properties-container w-full">
-                            <div v-for="(property, index) in properties" :key="index" class="property-container md:flex mb-4">
-                                <div>
-                                    <UInput 
-                                        v-model="property.key"
-                                        :name="'propertyKey' + index" 
-                                        placeholder="Property Key" 
-                                        class="mr-4" 
-                                    />
-                                </div>
-                                <div class="mt-4 md:mt-0">
-                                    <UInput 
-                                        v-model="property.value"
-                                        :name="'propertyValue' + index" 
-                                        placeholder="Property Value" 
-                                    />
-                                </div>
-                                <div class="flex mt-5 sm:mt-0">
-                                    <UButton @click="addProperty" class="sm:ml-4" icon="i-heroicons-plus" label="Add" />
-                                    <UButton @click="removeProperty(index)" class="ml-3" icon="i-heroicons-minus-small" color="red" label="Remove" />
+                    <div v-if="devicesList.length > 0">
+                        <div v-if="selectedOption === 'http'">
+                            <div class="url-container flex mb-4">
+                                <label class="text-sm font-medium text-gray-700 dark:text-gray-200" for="topic">URL:
+                                    <UInput name="url" />
+                                </label>
+                            </div>
+                        </div>
+
+                        <div v-if="selectedOption === 'mqtt'">                        
+                            <div class="topic-container flex mb-4">
+                                <label class="text-sm font-medium text-gray-700 dark:text-gray-200" for="topic">Topic:
+                                    <UInput name="topic" />
+                                </label>
+                            </div>
+                            <div class="retain-container mb-4">
+                                <label for="retain">
+                                    <UCheckbox name="retain" label="Retain" />
+                                </label>
+                            </div>
+                        </div>
+                        <div class="properties-element mb-4">
+                            <p>Properties:</p>
+                            <div class="properties-container w-full">
+                                <div v-for="(property, index) in properties" :key="index" class="property-container md:flex mb-4">
+                                    <div>
+                                        <UInput 
+                                            v-model="property.key"
+                                            :name="'propertyKey' + index" 
+                                            placeholder="Property Key" 
+                                            class="mr-4" 
+                                        />
+                                    </div>
+                                    <div class="mt-4 md:mt-0">
+                                        <UInput 
+                                            v-model="property.value"
+                                            :name="'propertyValue' + index" 
+                                            placeholder="Property Value" 
+                                        />
+                                    </div>
+                                    <div class="flex mt-5 sm:mt-0">
+                                        <UButton @click="addProperty" class="sm:ml-4" icon="i-heroicons-plus" label="Add" />
+                                        <UButton @click="removeProperty(index)" class="ml-3" icon="i-heroicons-minus-small" color="red" label="Remove" />
+                                    </div>
                                 </div>
                             </div>
                         </div>
